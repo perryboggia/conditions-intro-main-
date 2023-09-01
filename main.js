1b2f1d1ae6fa5114d84ce00863c85d03382e41e9
Perry Boggia <perryboggia@gmail.com>
2023-09-01 14:29:37 -0400

    your commit message

--- main.js
+++ main.js
@@ -1,3 +1,2 @@
-if ( 4 * 5 < 20 || 4 * 5 === 20) {console.log("True Fact")}
-let likedogs = "true"
-if (likedogs == "true") {console.log("you are a dog person");}
+if ( 4 * 5 < 20 || 4 * 5 === 20) {console.log("True False")}
+
